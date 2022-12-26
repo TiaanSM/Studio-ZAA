47bb6fad07299a75d570682d6fad14860d8f4b24
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-12-26 15:20:52 +0200

    Added horizontal scroll functionality.
    As seen on design scrolling up and down replaced with scrolling left and right.

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,27 @@ function App() {
 
   const [scrollRef, scrollPercentage] = useScrollPercentage();
 
+  let scrollValue = 0;
+
+  const handleScroll = (event) => {
+    scrollValue = event.deltaY;
+    console.log(scrollValue);
+    return scrollValue;
+  }
+
   return (
-    <div className="App" ref={scrollRef}>
+    <div className="App"  >
+
         <Navbar scrollProgress={scrollPercentage} />
+      <div className="scroll-container" ref={scrollRef} onWheel={handleScroll}>
+        <div className="content-container">
+
+        
         <Header />
         <Projects />
+
+        </div>
+      </div>
     </div>
   )
 }
