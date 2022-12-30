17b9756e2284d9858ff617ec9373ffc0ef14facb
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-12-30 15:04:23 +0200

    Completed basic layout for article.
    Article section desktop layout completed and added images for project.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,27 @@ import styles from './Contact.module.css';
 const Contact = () => {
   return (
     <div className={styles.contact}>
-      Contact
+
+      <div className={styles.container}>
+        <img src="" alt="" className={styles.image1} />
+        <p className={styles.articleTitle}>Achitecture And Change</p>
+        <a href="" className={styles.articleLink}>Read Article</a>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.articleContainer}>
+          <img src="" alt="" className={styles.image2} />
+          <p className={styles.articleTitle}>Achitecture And Change</p>
+          <a href="" className={styles.articleLink}>Read Article</a>
+        </div>
+
+        <div className={styles.articleContainer}>
+          <img src="" alt="" className={styles.image2} />
+          <p className={styles.articleTitle}>Achitecture And Change</p>
+          <a href="" className={styles.articleLink}>Read Article</a>
+        </div>
+
+      </div>
     </div>
   )
 }
