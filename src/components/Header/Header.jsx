775d21b9e74586577a8a1ecb2f6026283672f100
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-01-03 19:54:45 +0200

    Fixed minor responsive bugs & animations.
    Started with animation for intersection observer and fixed minor responsiveness bugs.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import styles from './Header.module.css';
 import headerImage from '../../assets/img/headerImg.webp';
+import { useInView } from 'react-intersection-observer';
 
 const Header = () => {
 
@@ -7,10 +8,14 @@ const Header = () => {
   // logo
   // top right desc
   // bottom title
+  const { ref, inView, entry } = useInView({
+    threshold: 0.40,
+    triggerOnce: true
+  });
 
   return (
    <header className={styles.header}>
-      <img className={styles.image} src={headerImage} alt="" />
+      <img className={styles.image} src={headerImage} alt="houseImage" ref={ref} />
       <h1 className={styles.title}>Building Beyond</h1>
       <p className={styles.desc}>We are a South African architecture and design studio with a global reputation for creating innovative spaces.</p>
    </header>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,13 +2,21 @@ import styles from './Projects.module.css';
 import image1 from '../../assets/img/projectImage4.webp';
 import image2 from '../../assets/img/projectImage5.webp';
 import image3 from '../../assets/img/projectImage6.webp';
+import { useInView } from 'react-intersection-observer';
 
 const Projects = () => {
+
+
+  const { ref, inView, entry } = useInView({
+    threshold: 0.40,
+    triggerOnce: true
+  });
+
   return (
     <main className={styles.main}>
 
         <div className={styles.container}>
-          <img src={image1} alt="" className={styles.projectImage} />
+          <img src={image1} alt="" className={inView ? styles.projectImage : styles.projectImageAnim} />
           <p className={styles.projectName}>Project name</p>
           <a href="" className={styles.link}>View Project</a>
         </div>
