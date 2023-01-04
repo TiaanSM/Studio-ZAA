24aaf42188c5551cc2d5ff45ea8d38c1b3682991
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-01-04 19:06:47 +0200

    Added zoom out animation.
    Zoom out animation when scrolled into view for desktop complete.

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -3,25 +3,40 @@ import articleImage from '../../assets/img/projectImage2.webp';
 import articleImage2 from '../../assets/img/articleImage2.webp';
 import articleImage3 from '../../assets/img/projectImage3.webp';
 
+import { useInView } from 'react-intersection-observer';
+
 const Articles = () => {
+
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: .80,
+    triggerOnce: true
+  });
+
   return (
     <div className={styles.articles}>
 
       <div className={styles.container}>
-        <img src={articleImage2} alt="" className={styles.image1} />
+      <div className={styles.imageContainer} ref={ref}>
+        <img src={articleImage2} alt="" className={inView ? styles.image1 : styles.image1A} />
+      </div>
         <p className={styles.articleTitle}>Achitecture And Change</p>
         <a href="" className={styles.articleLink}>Read Article</a>
       </div>
 
       <div className={styles.container}>
         <div className={styles.articleContainer}>
-          <img src={articleImage} alt="" className={styles.image2} />
+        <div className={styles.imageContainer2} >
+          <img src={articleImage} alt="" className={inView ? styles.image2 : styles.image2A} />
+        </div>
           <p className={styles.articleTitle}>Achitecture And Change</p>
           <a href="" className={styles.articleLink}>Read Article</a>
         </div>
 
         <div className={styles.articleContainer}>
-          <img src={articleImage3} alt="" className={styles.image2} />
+        <div className={styles.imageContainer2} >
+          <img src={articleImage3} alt="" className={inView ? styles.image2 : styles.image2A} />
+        </div>
           <p className={styles.articleTitle}>Achitecture And Change</p>
           <a href="" className={styles.articleLink}>Read Article</a>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,25 +3,40 @@ import image1 from '../../assets/img/projectImage4.webp';
 import image2 from '../../assets/img/projectImage5.webp';
 import image3 from '../../assets/img/projectImage6.webp';
 
+import { useInView } from 'react-intersection-observer';
+
 const Projects = () => {
 
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: .80,
+    triggerOnce: true
+  });
+
+  console.log(inView);
 
   return (
     <main className={styles.main}>
 
         <div className={styles.container}>
-          <img src={image1} alt="" className={styles.projectImage} />
+         <div className={styles.imageContainer} ref={ref}>
+          <img src={image1} alt="house-image" className={inView ? styles.projectImage : styles.projectImageA} />
+         </div>
           <p className={styles.projectName}>Project name</p>
           <a href="" className={styles.link}>View Project</a>
         </div>
         
         <div className={styles.container}>
-          <img src={image2} alt="" className={styles.projectImage}  />
+        <div className={styles.imageContainer} >
+          <img src={image2} alt="house-image" className={inView ? styles.projectImage : styles.projectImageA} />
+        </div>
           <p className={styles.projectName}>Project name</p>
           <a href="" className={styles.link}>View Project</a>         
         </div>
         <div className={styles.container}>
-          <img src={image3} alt="" className={styles.projectImage}  />
+          <div className={styles.imageContainer} >
+          <img src={image3} alt="house-image" className={inView ? styles.projectImage : styles.projectImageA} />
+          </div>
           <p className={styles.projectName}>Project name</p>
           <a href="" className={styles.link}>View Project</a>         
         </div>
