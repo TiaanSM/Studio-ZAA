43316aab8132a09b2c0aafdeaf85057a4160950d
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-01-15 19:44:38 +0200

    Article components fully responsive.
    Made the articles components for the pages fully responsive , still needs 2 minor layout adjustments.

--- src/components/Article/NextArticle.jsx
+++ src/components/Article/NextArticle.jsx
@@ -1,63 +1,28 @@
-import React from 'react'
+import styles from './Article.module.css';
 
 const nextArticle = (props) => {
 
-    const isMobile = window.innerWidth < 768;
-    // move to article css
-
-    const sectionStyle = {
-        width: isMobile ? '100vw':'1100px',
-        height: '100vh',
-        backgroundColor: '#3d4442',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: isMobile ? 'column' : 'row'
-    }
-
-    const imageContainerStyle = {
-        width: '60%',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-
-    const imageStyle = {
-        width: '500px',
-        height: '85vh',
-        backgroundColor: 'black',
-    };
 
     const textContainerStyle = {
-        width: '50%',
-        height: '85vh',
-        position: 'relative'
+        
     }
 
     const linkStyle = {
-        color: 'white',
-        fontFamily: 'sans-serif',
-        fontSize: '15px'
+        
     }
 
     const titleStyle = {
-        fontSize: '2rem',
-        fontWeight: '400',
-        color: 'white',
-        width: '15rem',
-        position: 'absolute',
-        bottom: '0'
+        
     }
 
   return (
-    <section style={sectionStyle}>
-        <div style={imageContainerStyle}>
-            <img src="" alt="" style={imageStyle} />
+    <section className={styles.nextSection}>
+        <div className={styles.nextImageContainer}>
+            <img src="" alt="" className={styles.nextImage} />
         </div>
-        <div style={textContainerStyle}>
-            <p style={linkStyle}>Next Article</p>
-            <h4 style={titleStyle}>{props.nextArticle}</h4>
+        <div className={styles.nextTextContainer}>
+            <p className={styles.nextLink}>Next Article</p>
+            <h4 className={styles.nextTitle}>{props.nextArticle}</h4>
         </div>
     </section>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,14 +6,11 @@ import Image from '../Image';
 
 const Header = () => {
 
-  // on website open = 3 box loading reveal, image zoom out, main text fade up.
-
-
   return (
    <header className={styles.header}>
       <a href="/"><img src={logo} alt="logo" className={styles.logo} /></a>
       <div className={styles.imageContainer}>
-      <img className={styles.image} src={headerImage} alt="house-image"/>
+        <img className={styles.image} src={headerImage} alt="house-image"/>
       </div>
       <h1 className={styles.title}>Building Beyond</h1>
       <p className={styles.desc}>We are a South African architecture and design studio with a global reputation for creating innovative spaces.</p>
