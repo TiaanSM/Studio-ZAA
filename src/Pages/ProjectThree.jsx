25dd90456404a3a6a8943f30886d5bd44e1066e2
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-01-15 18:32:20 +0200

    Projects components fully responsive.
    Made the components for each of the projects responsive for all devices.

--- src/Pages/ProjectThree.jsx
+++ src/Pages/ProjectThree.jsx
@@ -3,7 +3,6 @@ import ProjectSidebar from '../components/Project/ProjectSidebar';
 import ProjectImages from '../components/Project/ProjectImages';
 
 import styles from '../components/Project/Project.module.css';
-import logo from '../assets/img/Logo.png';
 
 import useScrollPercentage from '../components/useScrollPercentage';
 
@@ -11,19 +10,20 @@ import useScrollPercentage from '../components/useScrollPercentage';
 const ProjectThree = () => {
 
   const [scrollRef, scrollPercentage] = useScrollPercentage();
+  const isMobile = window.innerWidth < 768;
 
   return (
     <section className={styles.section} ref={scrollRef}>
-      <a href="/"><img src={logo} alt="logo" className={styles.logo} /></a>
       <Navbar scrollProgress={scrollPercentage} />
+      {isMobile ? <ProjectImages height="80vh"/> : null}
       <ProjectSidebar 
         title="Wineland Golf Field" 
         desc="Zil Pasyon's architecture is designed and built to settle discretely among the Islands unique granite boulders. The phrase, '“We will have succeeded if nothing can be seen from outside the Island” was a starting point for this project which began with a Concept Design in 2005, reaching final completion in 2017. The Six Senses resort in the Seychelles Islands on the private island of Félicité comprises 15 Resort Villas with their own infinity pool and ocean view, 3 Oceanside Bars, its own helicopter pad as well as a small number of luxury hillside Private Residences, each with their own acrylic base pool." 
         location="Port Elizabeth"
         completion="2021"
       />
-      <ProjectImages />
-      <ProjectImages />
+      <ProjectImages height="100vh" />
+      <ProjectImages height="100vh" />
     </section>
   )
 }
--- src/components/Project/ProjectImages.jsx
+++ src/components/Project/ProjectImages.jsx
@@ -1,12 +1,52 @@
-import styles from './Project.module.css';
+
 
 const ProjectImages = (props) => {
 
+  const isMobile = window.innerWidth < 768;
+
+  const containerStyles = {
+    width: isMobile ? props.width : 'calc(100vw - 3rem - 300px)',
+    height: isMobile ? props.height : '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor:'#333'
+  };
+
+  const imageStyles = {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
+  }
+
   return (
-    <div className={styles.imageContainer}>
-        <img src={props.src} alt={props.alt} className={styles.image} />
+    <div style={containerStyles}>
+        <img src={props.src} alt={props.alt} style={imageStyles} />
     </div>
   )
 }
 
 export default ProjectImages
+
+/*
+
+
+.image {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+} 
+
+
+
+.imageContainer {
+        width: 100vw;
+        height: 500px;
+    }
+
+    .image {
+        width: 100%;
+        height: 100%;
+    }
+*/
