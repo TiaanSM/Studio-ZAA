cd9b3c4e40d9b9c1c9d5e7fbc26720228a5d510a
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-02-27 13:04:11 +0200

    Removed unused images, fixed bugs.
    Removed images and image components that are not used and improved mobile responsiveness.

--- src/Pages/ProjectOne.jsx
+++ src/Pages/ProjectOne.jsx
@@ -35,7 +35,6 @@ const ProjectOne = () => {
     <section className={styles.section} ref={scrollRef}>
       <PageChange pageLoaded={playAnimation} />
       <Navbar scrollProgress={scrollPercentage} />
-      {isMobile ? <ProjectImages height="80vh"/> : null}
       <ProjectSidebar 
         title="Malbury Ocean View" 
         desc="Zil Pasyon's architecture is designed and built to settle discretely among the Islands unique granite boulders. The phrase, '“We will have succeeded if nothing can be seen from outside the Island” was a starting point for this project which began with a Concept Design in 2005, reaching final completion in 2017. The Six Senses resort in the Seychelles Islands on the private island of Félicité comprises 15 Resort Villas with their own infinity pool and ocean view, 3 Oceanside Bars, its own helicopter pad as well as a small number of luxury hillside Private Residences, each with their own acrylic base pool." 
